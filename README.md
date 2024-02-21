049b2e2b0d8f6dcc08be0f42636c7e913fa48b9b
Vaibhav Kumar <vk11112003vk@gmail.com>
2024-02-21 22:48:08 +0530

    This is last project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Boston_House_Prcing
 using this 
+
+Create the Environment 
+# conda create -p venv python == 3.7 -y
--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run()
    
      
